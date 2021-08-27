9bd77ac94ab7559b389b2995a3c0c0560d447697
Diana Rand <diana.rand@outlook.com>
2021-08-27 19:16:29 +0300

    initial layout

--- app.py
+++ app.py
@@ -4,9 +4,39 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/books', methods=['GET'])
+def get_booklist():  # Get a list of books
+    pass
+
+
+@app.route('/books', methods=['POST'])
+def add_book():  # Create a new book
+    pass
+
+
+@app.route('/book/<int:book_id>', methods=['GET'])
+def get_book():  # Get a book
+    pass
+
+
+@app.route('/book/<int:book_id>', methods=['DELETE'])
+def remove_book():  # Remove a book
+    pass
+
+
+@app.route('/reserve', methods=['POST'])
+def reserve_book():  # Reserve a book or cancel a reservation
+    pass
+
+
+@app.route('lend', methods=['POST'])
+def lend_book():  # Lend or return a book
+    pass
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
