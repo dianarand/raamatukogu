ab6abcc7761728265eff4030897a206ee2a8d1de
Diana Rand <diana.rand@outlook.com>
2021-08-28 20:57:34 +0300

    added SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+# from db import db
+from models import Book
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
 
 
 @app.route('/books', methods=['GET'])
@@ -22,62 +30,29 @@ def get_book_list():  # Get a list of books
         books.append(book)
 
     connection.close()
-    return jsonify({'books': books})
+    return {'books': books}
 
 
 @app.route('/books', methods=['POST'])
 def add_book():  # Create a new book
     data = request.get_json()
-
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = 'INSERT INTO books VALUES (NULL, ?, ?, ?, ?)'
-    cursor.execute(query, (data['title'], data['author'], data['year'], 1))
-
-    connection.commit()
-    connection.close()
-
+    book = Book(data['title'], data['author'], data['year'], 1)
+    book.save_to_db()
     return {'message': 'book added successfully'}, 201
 
 
 @app.route('/book/<int:book_id>', methods=['GET'])
 def get_book(book_id):  # Get a book
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = 'SELECT * FROM books WHERE id=?'
-    result = cursor.execute(query, (book_id,))
-    row = result.fetchone()
-
-    if row:
-        book = {
-            "id": row[0],
-            "title": row[1],
-            "author": row[2],
-            "year": row[3],
-            "active": bool(row[4])
-        }
-    else:
-        book = None
-
-    connection.close()
-
-    return book
+    result = Book.query.filter_by(id=book_id).first()
+    return result.json()
 
 
 @app.route('/book/<int:book_id>', methods=['DELETE'])
 def remove_book(book_id):  # Remove a book
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    query = 'UPDATE books SET active=0 WHERE id=?'
-    cursor.execute(query, (book_id,))
-
-    connection.commit()
-    connection.close()
-
-    return {'message': 'book removed successfully'}
+    result = Book.query.filter_by(id=book_id).first()
+    result.active = 0
+    result.save_to_db()
+    return result.json()
 
 
 @app.route('/reserve', methods=['POST'])
--- models.py
+++ models.py
@@ -1,21 +1,53 @@
-class Book:
-    def __init__(self, _id, title, author, year):
-        self.id = _id
+from db import db
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    author = db.Column(db.String(80))
+    year = db.Column(db.Integer)
+    active = db.Column(db.Integer)
+
+    def __init__(self, title, author, year, active):
         self.title = title
         self.author = author
         self.year = year
-        self.active = True
+        self.active = active
+
+    def json(self):
+        result = {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'year': self.year,
+            'active': bool(self.active)
+        }
+        return result
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
 
-class LendedBook():
+class LentBook:
     pass
 
 
-class ReservedBook():
+class ReservedBook:
     pass
 
 
-class User:
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    lender = db.Column()
+    borrower = db.Column()
+
     def __init__(self, _id, username, password, lender, borrower):
         self.id = _id
         self.username = username
