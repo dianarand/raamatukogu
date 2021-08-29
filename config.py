78d612f12d643cc4fd77b6d2ba74f6973d3cfed8
Diana Rand <diana.rand@outlook.com>
2021-08-29 19:03:39 +0300

    added better secret key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 
 class Config:
-    SECRET_KEY = 'DIANA'
+    SECRET_KEY = '67935bee40960ace7c329c0cd8848a3d'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_AUTH_URL_RULE = '/login'
